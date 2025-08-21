adc6611aea7868b9da260ef236eac4badd022e27
jaeilssss <jaeilssss@bitbucket.org>
2025-08-21 11:58:18 +0900

    latest update

--- PaymentService/src/main/kotlin/com/ecommercial/shopping/paymentservice/payment/domain/entity/Payment.kt
+++ PaymentService/src/main/kotlin/com/ecommercial/shopping/paymentservice/payment/domain/entity/Payment.kt
@@ -1,4 +1,19 @@
 package com.ecommercial.shopping.paymentservice.payment.domain.entity
 
-class Payment {
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "payment")
+class Payment(
+
+    @Id @GeneratedValue
+    @Column(name = "pay_id")
+    val id: Long,
+    val userId: Long,
+    val totalPrice: Int
+) {
 }
