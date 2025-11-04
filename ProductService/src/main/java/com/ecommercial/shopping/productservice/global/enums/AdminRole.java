4a6aed374f39ace111a9b5e06e5877bfe83f6ec2
jaeilssss <jaeilssss@bitbucket.org>
2025-11-04 12:46:29 +0900

    feat : 상품 등록 시 권한 체크 로직 추가

--- ProductService/src/main/java/com/ecommercial/shopping/productservice/global/enums/AdminRole.java
+++ ProductService/src/main/java/com/ecommercial/shopping/productservice/global/enums/AdminRole.java
@@ -1,15 +1,31 @@
 package com.ecommercial.shopping.productservice.global.enums;
 
 
+import com.ecommercial.shopping.productservice.global.error.JwtTokenError;
+import com.ecommercial.shopping.productservice.global.exception.MyException;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public enum AdminRole {
-    STAFF("스태프"),
-    MANAGER("중간 관리자"),
-    SUPER_ADMIN("최고 관리자");
+    STAFF("스태프", "ROLE_STAFF"),
+    MANAGER("중간 관리자", "ROLE_MANAGER"),
+    SUPER_ADMIN("최고 관리자", "ROLE_SUPER_ADMIN");
 
     private String role;
+    private String roleValue;
 
+    public static AdminRole fromRole(String role) {
+        for(AdminRole adminRole : AdminRole.values()) {
+            if(adminRole.role.equals(role)) {
+                return adminRole;
+            }
+        }
+        throw new MyException(
+                JwtTokenError.IN_APPROPRIATE_TOKEN.getHttpStatus(),
+                JwtTokenError.IN_APPROPRIATE_TOKEN.getMessage()
+        );
+    }
 }
 
