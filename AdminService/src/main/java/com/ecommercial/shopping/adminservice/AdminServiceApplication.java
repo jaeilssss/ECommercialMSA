88180fafd6a2e447cefff69ad34528d8523f6f7b
jaeilssss <jaeilssss@bitbucket.org>
2025-12-11 15:05:08 +0900

    deply with kube, argocd

--- AdminService/src/main/java/com/ecommercial/shopping/adminservice/AdminServiceApplication.java
+++ AdminService/src/main/java/com/ecommercial/shopping/adminservice/AdminServiceApplication.java
@@ -10,7 +10,7 @@ public class AdminServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminServiceApplication.class, args);
-        System.out.println("test123456");
+        System.out.println("test1233456");
     }
 
 }
