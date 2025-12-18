b197041cbb2ecb9ba9e69a884c4fe431f8b3971a
jaeilssss <jaeilssss@bitbucket.org>
2025-12-18 12:12:47 +0900

    feat: elastic search update를 위한 model 구현

---
+++ event-common/src/main/java/com/ecommercial/shopping/model/UpdateElasticSearchProductMessage.java
@@ -0,0 +1,15 @@
+package com.ecommercial.shopping.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class UpdateElasticSearchProductMessage {
+    private Long productId;
+    private String productName;
+    private String companyName;
+    private String categoryName;
+}
